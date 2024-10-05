9a7b3bf3b798612c34669079beb6f69e03eaf125
Prateek <prateekj1211@gmail.com>
2024-10-06 01:58:45 +0530

    started adding npm modules, prettier and exlint

--- const.mjs
+++ const.mjs
@@ -1,11 +1,9 @@
 const countryTimeZones = {
-    "US": "America/New_York",
-    "IN": "Asia/Kolkata",
-    "JP": "Asia/Tokyo",
-    "AU": "Australia/Sydney",
-    "GB": "Europe/London"
-};
-
-export {
-    countryTimeZones
+    US: 'America/New_York',
+    IN: 'Asia/Kolkata',
+    JP: 'Asia/Tokyo',
+    AU: 'Australia/Sydney',
+    GB: 'Europe/London',
 }
+
+export { countryTimeZones }
--- script.mjs
+++ script.mjs
@@ -1,13 +1,12 @@
-import { clock } from "./clock.mjs";
+import { clock } from './clock.mjs'
 
+function populateDisplay(countryCode = 'IN') {
+    let time = clock(countryCode)
+    let clock_display = document.querySelector('.clock-box')
+    let date_display = document.querySelector('.date-box')
 
-function populateDisplay(countryCode="IN") {
-    let time = clock(countryCode);
-    let clock_display = document.querySelector(".clock-box");
-    let date_display = document.querySelector(".date-box");
-    
-    clock_display.innerText = time.time;
-    date_display.innerText = time.date;
+    clock_display.innerText = time.time
+    date_display.innerText = time.date
 }
 
-setInterval(populateDisplay,1000);
+setInterval(populateDisplay, 1000)
