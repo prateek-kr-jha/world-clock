df2e853b5fe6584919c03e3128e6bd0f97d8bbbe
Prateek <prateekj1211@gmail.com>
2024-10-06 01:32:18 +0530

    added locales for different countries

---
+++ const.mjs
@@ -0,0 +1,11 @@
+const countryTimeZones = {
+    "US": "America/New_York",
+    "IN": "Asia/Kolkata",
+    "JP": "Asia/Tokyo",
+    "AU": "Australia/Sydney",
+    "GB": "Europe/London"
+};
+
+export {
+    countryTimeZones
+}
---
+++ script.mjs
@@ -0,0 +1,44 @@
+import { countryTimeZones } from "./const.mjs"
+
+function clock(countryCode) {
+    if(!countryTimeZones) {
+        throw new Error("country code can't be empty.");
+    } 
+    countryCode = countryCode.toUpperCase();
+
+    if(!countryTimeZones[countryCode]) {
+        throw new Error("No config for this country.");
+    }
+    let currentTimestamp = Date.now();
+    let currentTime = new Intl.DateTimeFormat('en-IN', {
+        timeZone: countryTimeZones[countryCode.toUpperCase()],
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric', 
+        hourCycle: 'h23'
+    });
+    let currentDate = new Intl.DateTimeFormat('en-IN', {
+        timeZone: countryTimeZones[countryCode.toUpperCase()],
+        year: "numeric",
+        month: "numeric",
+        day: "numeric"
+
+    });
+    
+    
+    return {
+        time: currentTime.format(currentTimestamp),
+        date: currentDate.format(currentTimestamp)
+    };
+
+}
+
+
+// setInterval(clock, 1000);
+
+console.log(clock("FR"));
+
+// TODO: Clock
+// TODO: TOGGLE for dark and light mode
+// TODO: dropdown to select country
+
