ed61d1f250cca08be01da4d10b52d16a2240d033
Prateek <prateekj1211@gmail.com>
2024-10-19 18:09:43 +0530

    added script

--- playground/modal/script.js
+++ playground/modal/script.js
@@ -1,1 +1,24 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
 
+
+const openModal = ()=> {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+const closeModal = ()=>{
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+openModalBtn.addEventListener("click", openModal);
+closeModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", (e) => {
+    if(e.key == "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+})
