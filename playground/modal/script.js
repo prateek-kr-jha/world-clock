9b92e9fce114abddf06f5bc4e083da173a2d5853
Prateek <prateekj1211@gmail.com>
2024-10-19 18:36:34 +0530

    setup dropdown folder

--- playground/modal/script.js
+++ playground/modal/script.js
@@ -1,24 +1,23 @@
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openModalBtn = document.querySelector('.btn-open');
+const closeModalBtn = document.querySelector('.btn-close');
 
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
 
-const openModal = ()=> {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-}
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
 
-const closeModal = ()=>{
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-}
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
 
-openModalBtn.addEventListener("click", openModal);
-closeModalBtn.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", (e) => {
-    if(e.key == "Escape" && !modal.classList.contains("hidden")) {
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
-})
+});
