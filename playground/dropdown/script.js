9f8be40d87d8d0fc61f43ef63f743ff695577bf0
Prateek <prateekj1211@gmail.com>
2024-10-25 00:38:43 +0530

    formatted the code

--- playground/dropdown/script.js
+++ playground/dropdown/script.js
@@ -1,16 +1,19 @@
-const imgs = document.querySelectorAll('.dropdown a');
-// console.log(imgs);
+function addEffect() {
+    const imgs = document.querySelectorAll('.dropdown a');
 
-imgs.forEach((img) => {
-    img.addEventListener('mouseover', (e) => {
-        console.log(e.currentTarget.querySelector('svg'));
-        e.currentTarget.querySelector('svg').style.fill = 'white';
+    imgs.forEach((img) => {
+        img.addEventListener('mouseover', (e) => {
+            console.log(e.currentTarget.querySelector('svg'));
+            e.currentTarget.querySelector('svg').style.fill = 'white';
+        });
     });
-});
 
-imgs.forEach((img) => {
-    img.addEventListener('mouseout', (e) => {
-        console.log(e.currentTarget.querySelector('svg'));
-        e.currentTarget.querySelector('svg').style.fill = '#434343';
+    imgs.forEach((img) => {
+        img.addEventListener('mouseout', (e) => {
+            console.log(e.currentTarget.querySelector('svg'));
+            e.currentTarget.querySelector('svg').style.fill = '#434343';
+        });
     });
-});
+}
+
+addEffect();
