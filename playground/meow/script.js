2bd2f9529f2802b106aa7982fc637c42b411e214
prateek <prateek.jha@yulu.bike>
2024-10-24 20:58:46 +0530

    added background overlay

--- playground/meow/script.js
+++ playground/meow/script.js
@@ -4,12 +4,16 @@ function modal() {
     const open_btn = document.querySelector('.btn');
     const modal = document.querySelector('.modal');
     const close_btn = document.querySelector('.close-btn');
+    const overlay = document.querySelector('.overlay');
+
     open_btn.addEventListener('click', () => {
         modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
     });
-    
+
     close_btn.addEventListener('click', () => {
         modal.classList.add('hidden');
+        overlay.classList.add('hidden');
     });
 }
 
