9f069b630447f580e2118f1a6a0bf64e320b7590
Prateek <prateekj1211@gmail.com>
2024-10-25 00:59:31 +0530

    added dropdwon effect

--- playground/dropdown/script.js
+++ playground/dropdown/script.js
@@ -1,19 +1,36 @@
-function addEffect() {
+function addHoverEffect() {
     const imgs = document.querySelectorAll('.dropdown a');
 
     imgs.forEach((img) => {
         img.addEventListener('mouseover', (e) => {
-            console.log(e.currentTarget.querySelector('svg'));
             e.currentTarget.querySelector('svg').style.fill = 'white';
         });
     });
 
     imgs.forEach((img) => {
         img.addEventListener('mouseout', (e) => {
-            console.log(e.currentTarget.querySelector('svg'));
             e.currentTarget.querySelector('svg').style.fill = '#434343';
         });
     });
 }
 
+function addDropdownEffect() {
+    const dropDownBtn = document.querySelector('.btn');
+    const dropDown = document.querySelector('.dropdown');
+    const toggleArrow = document.getElementById('arrow');
+    const toggleDropdown = () => {
+        dropDown.classList.toggle('show');
+        toggleArrow.classList.toggle('arrow');
+    };
+
+    dropDownBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        toggleDropdown();
+    });
+}
+
+function addEffect() {
+    addHoverEffect();
+    addDropdownEffect();
+}
 addEffect();
